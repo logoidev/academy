86caf7835a14d88fc2d2b49e132891b5dbb02f5e
Vlad Kolbaia <vlad.kolbaia@gmail.com>
2022-08-11 14:37:34 +0200

    Quick snake implementation + canvas testing (#4)

---
+++ vlads-solutions/snake/__tests__/__mocks__/canvas.js
@@ -0,0 +1,27 @@
+import { expect, vi } from "vitest";
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+import { createCanvas, Canvas } from "canvas";
+
+export const canvasToBuffer = (canvas) => {
+  const base64 = canvas.toDataURL().split(",")[1];
+  return Buffer.from(base64, "base64");
+};
+
+expect.extend({
+  toMatchImageSnapshot: function toMatchImageSnapshot(imageOrCanvas) {
+    const isCanvas = imageOrCanvas instanceof Canvas;
+    const normalised = isCanvas ? canvasToBuffer(imageOrCanvas) : imageOrCanvas;
+    const result = toMatchImageSnapshot.bind(this)(normalised);
+    return result;
+  },
+});
+
+export const mockCanvasGetter = () => {
+  const getElementById = document.getElementById;
+  document.getElementById = vi.fn((id) => {
+    if (id === "canvas") {
+      return createCanvas();
+    }
+    return getElementById(id);
+  });
+};
