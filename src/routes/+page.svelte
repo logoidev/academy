9ae06ca0ee3bf3991d0347bdd6294b00b05fb0dc
Vlad Kolbaia <v@logoi.dev>
2023-02-01 14:51:48 -0500

    Main page of Academy minor change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,52 @@
 
 	const MAILTO_URL = `mailto:academy@logoi.dev?subject=Expressing interest in Logoi Academy`;
 
+	let isMottoShown = false;
+
+	let apprenticeshipIndex = 0;
+
+	let ms = 3333;
+	let index = 0;
+	const next = () => {
+		index += 1;
+		if (index > ACADEMY.length - 1) {
+			index = 0;
+		}
+	};
+
+	let clear: Timer;
+	$: {
+		clearInterval(clear);
+		clear = setInterval(next, ms);
+	}
+
 	onMount(() => {
 		if (window.location.search) {
 			window.location.href = window.location.pathname;
 		}
 	});
+
+	$: MAILTO = MAILTO_URL + `, ${APPRENTICESHIPS[apprenticeshipIndex]} Apprenticeship`;
+
+	const LANGUAGES = ['English', 'Greek', 'Ukrainian', 'Russian', 'Georgian', 'Hebrew'];
+	const ACADEMY = ['Academy', 'Ακαδημία', 'Академія', 'Aкадемия', 'აკადემია', 'אֲקָדֶמִיָה'];
+	const MOTTO = [
+		'Let no one untrained in geometry enter.',
+		'Ἀγεωμέτρητος μηδεὶς εἰσίτω',
+		'Не геометр, та не ввійде',
+		'Не геометр да не войдёт!',
+		'გეომეტრიის უცოდინარი არ შეიძლება შემოვიდეს',
+		'אף אחד שלא יודע בגיאומטריה רשאי להיכנס'
+	];
+
+	const SUMMARY = ['Dialectic approach to Software Engineering', 'Practical group learning'];
+
+	const APPRENTICESHIPS = [
+		'Software Engineeering',
+		'Digital Design',
+		'Web Development',
+		'Game Development'
+	];
 </script>
 
 <div
@@ -24,19 +65,53 @@
 		<div class="w-80">
 			<Image src={LogoSrc} />
 		</div>
-		<div class="text-xl">
-			<span>Academy</span>
+
+		<div class="flex flex-col items-center h-20">
+			<button class="text-xl" on:click={() => (isMottoShown = !isMottoShown)}>
+				<span class="trajan text-3xl">{ACADEMY[index]}</span>
+			</button>
+
+			{#if isMottoShown}
+				<div class="text-xl">
+					<span class="text-sm trajan">"{MOTTO[index]}"</span>
+				</div>
+			{/if}
 		</div>
+
+		{#each SUMMARY as line}
+			<div class="text-lg">{line}</div>
+		{/each}
+
 		<div class="text-sm mt-8 flex flex-col items-center">
-			<span>Digital Design & Software Engineeering</span>
-			<span>Aprenticeship</span>
+			<div class="w-48 flex text-center">
+				<button
+					class="text-xl p-4"
+					disabled={apprenticeshipIndex === 0}
+					on:click={() => {
+						if (apprenticeshipIndex > 0) {
+							apprenticeshipIndex--;
+						}
+					}}>{'<'}</button
+				>
+				<span class="text-lg">{APPRENTICESHIPS[apprenticeshipIndex] ?? ''} Aprenticeship</span>
+				<button
+					class="text-xl p-4"
+					disabled={apprenticeshipIndex === APPRENTICESHIPS.length - 1}
+					on:click={() => {
+						if (apprenticeshipIndex < APPRENTICESHIPS.length - 1) {
+							apprenticeshipIndex++;
+						}
+					}}>{'>'}</button
+				>
+			</div>
 		</div>
-		<div class="call-to-action text-sm">
-			<a href={MAILTO_URL} target="__blank">Get in touch</a>
+
+		<div class="call-to-action text-lg">
+			<a href={MAILTO} target="__blank">Get in touch</a>
 		</div>
 	</div>
 	<div class="flex flex-col items-center">
-		<div class="text-sm underline">
+		<div class="text-base underline">
 			<a href="/members">Members</a>
 		</div>
 
@@ -54,7 +129,7 @@
 
 <style>
 	div.wrapper {
-		font-family: 'e-Ukraine', sans-serif;
+		font-family: ui-serif, serif;
 	}
 
 	.call-to-action a {
@@ -69,5 +144,9 @@
 
 	.call-to-action a:hover {
 		background-color: lightgrey;
+	}
+
+	.trajan {
+		font-family: 'Trajan Pro', serif;
 	}
 </style>
